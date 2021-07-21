f7b976006b5c2c1fb99866fe013a07a4bfe12313
minung.han <hmu332233@gmail.com>
2021-07-22 00:12:36 +0900

    feat: Layout dom 구조 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -11,10 +11,8 @@ function Layout({ children }: LayoutProps) {
     <div className="flex flex-col min-h-screen">
       <Header title="JBNU BLIND" />
       <main className="flex-grow">
-        <section className="text-gray-700 body-font overflow-hidden">
-          <div className="container flex flex-col px-5 py-12 mx-auto max-w-screen-sm">
-            {children}
-          </div>
+        <section className="text-gray-700 body-font container flex flex-col px-5 pt-12 pb-24 mx-auto max-w-screen-sm">
+          {children}
         </section>
       </main>
     </div>
