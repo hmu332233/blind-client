cb164cd1ea23db68af1beba2c527f3ead747218c
minung.han <hmu332233@gmail.com>
2021-07-22 00:19:08 +0900

    feat: Input에 onSubmit props 추가

--- src/components/PostInput/index.tsx
+++ src/components/PostInput/index.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
 type PostInputProps = {
-  onChange: (value: string) => void;
-  author: string,
+  onSubmit: (value: string) => void;
 };
 
-function PostInput({ onChange, author }: PostInputProps) {
+function PostInput({ onSubmit }: PostInputProps) {
   const [isButtonShown, setIsButtonShown] = useState(false);
   const [text, setText] = useState('');
 
@@ -14,19 +13,34 @@ function PostInput({ onChange, author }: PostInputProps) {
     setText(e.target.value);
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      onSubmit(text);
+      setText('');
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit(text);
+    setText('');
+  };
+
   useEffect(() => {
     setIsButtonShown(!!text);
   }, [text])
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="flex items-center fixed bottom-0 left-0 w-full border-t-2 p-4 bg-white">
         <textarea
           rows={2}
           className="block w-full focus:border-none"
           placeholder="댓글을 적어주세요."
-          defaultValue={''}
+          value={text}
           onChange={handleChange}
+          onKeyDown={handleKeyDown}
         />
         {isButtonShown && (
           <button className="ml-2 flex-shrink-0 font-bold p-2">등록</button>
@@ -37,8 +51,7 @@ function PostInput({ onChange, author }: PostInputProps) {
 }
 
 PostInput.defaultProps = {
-  onChange: () => { },
-  author: '',
+  onSubmit: () => {},
 };
 
 export default PostInput;
