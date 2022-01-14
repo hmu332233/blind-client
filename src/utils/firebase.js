bbc51fcdd7c823dc1bd71fee401b6ed02ccb5cf3
minung.han <hmu332233@gmail.com>
2022-01-15 02:24:56 +0900

    chore: firebase에 .env 사용하도록 변경

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,10 +1,12 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-firebase.initializeApp(firebaseConfig);
-
-export const db = firebase.firestore();
-export default firebase;
+export const app = initializeApp(firebaseConfig);
