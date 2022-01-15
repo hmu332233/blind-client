fef62f8898cb33c1f72d206491b492705019fd7a
minung.han <hmu332233@gmail.com>
2022-01-16 02:00:24 +0900

    feat: Outlet 기반으로 Layout 컴포넌트 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
+import { Outlet } from "react-router-dom";
 
 import Header from '../Header';
 
-type LayoutProps = {
-  children: React.ReactNode,
-};
-
-function Layout({ children }: LayoutProps) {
+function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header title="JBNU BLIND" />
       <main className="flex-grow">
         <section className="text-gray-700 body-font container flex flex-col px-5 py-12 mx-auto max-w-screen-sm">
-          {children}
+          <Outlet />
         </section>
       </main>
     </div>
   );
 }
-
-Layout.defaultProps = {
-};
 
 export default Layout;
