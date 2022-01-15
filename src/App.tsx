6d2e272e5d390bf5c4b7fd080b5014057be80bda
minung.han <hmu332233@gmail.com>
2022-01-16 02:05:46 +0900

    feat: 로그인 처리와 Layout이 적용된 App으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,35 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import { app, auth } from './utils/firebase';
 
 import MainPage from './pages/MainPage';
-import EntryPage from './pages/EntryPage';
+import SignInPage from './pages/SignIn';
 import SignInRedirect from './pages/SignInRedirect';
+import SignInComplete from './pages/SignInComplete';
 import Layout from './components/Layout';
+import RequireAuth from './components/RequireAuth';
 
 import { AuthProvider } from './contexts/AuthContext';
-
 
 function App() {
   return (
     <Router>
-        <AuthProvider>
+      <AuthProvider>
         <Routes>
-          <Route path="/signin/redirect" element={<SignInRedirect />} />
-            <Route path="/" element={<Layout />}>
-                <Route path="/entry" element={<EntryPage />} />
-                <Route index element={<MainPage />} />
-            </Route>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <RequireAuth>
+                  <MainPage />
+                </RequireAuth>
+              }
+            />
+            <Route path="/signin" element={<SignInPage />} />
+            <Route path="/signin/complete" element={<SignInComplete />} />
+          </Route>
+            <Route path="/signin/redirect" element={<SignInRedirect />} />
           {/* <Route path="*" element={<NoMatch />} /> */}
         </Routes>
-        </AuthProvider>
+      </AuthProvider>
     </Router>
   );
 }
