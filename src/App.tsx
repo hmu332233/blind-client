560315748b4c45838ef2a94111b59ea93484b86e
minung.han <hmu332233@gmail.com>
2022-01-16 00:10:52 +0900

    feat: 이메일 링크 로그인을 위한 redirect 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import './App.css';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import { app, auth } from './utils/firebase';
 
 import MainPage from './pages/MainPage';
 import EntryPage from './pages/EntryPage';
+import SignInRedirect from './pages/SignInRedirect';
 import Layout from './components/Layout';
 
-import { NameProvider } from './contexts/NameContext';
+import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <NameProvider>
-          <Routes>
-            <Route path="/entry" element={<EntryPage />} />
-            <Route path="/" element={<MainPage />} />
-          </Routes>
-        </NameProvider>
-      </Layout>
+        <AuthProvider>
+        <Routes>
+          <Route path="/signin/redirect" element={<SignInRedirect />} />
+            <Route path="/" element={<Layout />}>
+                <Route path="/entry" element={<EntryPage />} />
+                <Route index element={<MainPage />} />
+            </Route>
+          {/* <Route path="*" element={<NoMatch />} /> */}
+        </Routes>
+        </AuthProvider>
     </Router>
   );
 }
---
+++ src/pages/SignInRedirect/index.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+
+import { auth } from '../../utils/firebase';
+import { isSignInWithEmailLink, signInWithEmailLink } from 'firebase/auth';
+
+function SignInRedirect() {
+  useEffect(() => {
+    // 로그인 이메일 링크인지 체크
+    if (isSignInWithEmailLink(auth, window.location.href)) {
+      // Additional state parameters can also be passed via URL.
+      // This can be used to continue the user's intended action before triggering
+      // the sign-in operation.
+      // Get the email if available. This should be available if the user completes
+      // the flow on the same device where they started it.
+      let email = window.localStorage.getItem('emailForSignIn');
+      if (!email) {
+        // User opened the link on a different device. To prevent session fixation
+        // attacks, ask the user to provide the associated email again. For example:
+        email = window.prompt('인증을 위해 이메일을 한번 더 입력해주세요!');
+      }
+      // The client SDK will parse the code from the link for you.
+      signInWithEmailLink(auth, email, window.location.href)
+        .then((result) => {
+          // Clear email from storage.
+          window.localStorage.removeItem('emailForSignIn');
+          // You can access the new user via result.user
+          // Additional user info profile not available via:
+          // result.additionalUserInfo.profile == null
+          // You can check if the user is new or existing:
+          // result.additionalUserInfo.isNewUser
+        })
+        .catch((error) => {
+          // Some error occurred, you can inspect the code: error.code
+          // Common errors could be invalid email and invalid or expired OTPs.
+          console.error({ ...error });
+        });
+    }
+  }, []);
+
+  return <div>Sign in...</div>;
+}
+
+export default SignInRedirect;
