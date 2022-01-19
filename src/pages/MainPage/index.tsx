9867405cec1dbda609bf5fd89de2a11ed7c50304
minung.han <hmu332233@gmail.com>
2022-01-20 02:56:16 +0900

    feat: infinity scroll을 통한 firestore 호출 추가

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,40 +1,62 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 
 import PostCardList from '../../components/PostCardList';
 import PostInput from '../../components/PostInput';
-import useFirestore from '../../hooks/useFirestore';
-import { PostCardProps } from '../../components/PostCard';
 
-import { NameStateContext } from '../../contexts/NameContext';
+import { AuthStateContext, AuthActionContext } from '../../contexts/AuthContext';
 
-type Post = {
-  id?: string,
-  contents: string,
-  author: string,
-};
+import { addDoc, getDocs, query, orderBy, limit, startAfter, QueryDocumentSnapshot } from "firebase/firestore";
+
+import { postsRef } from '../../utils/firebase/firebase';
+import LoadingBox from '../../components/LoadingBox';
+
 
 function MainPage() {
-  const name = useContext(NameStateContext);
-  const { items: posts, add } = useFirestore<Post>({ key: 'posts' });
+  const user = useContext(AuthStateContext);
+  const { signout } = useContext(AuthActionContext);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const lastPostSnapshot = useRef<QueryDocumentSnapshot>();
+  const [isLast, setIsLast] = useState(false);
 
-  const handleSubmit = (value: string) => {
-    add({
+  const handleSubmit = async (value: string) => {
+    const post: Post = {
+      id: `p_${Date.now()}`,
       contents: value,
-      author: name,
-    });
+      creatorId: user.id,
+      creatorName: '아무거나',
+      createdAt: Date.now(),
+    };
+
+    await addDoc(postsRef, post);
+    setPosts(originPosts => [post, ...originPosts]);
+  };
+
+  const getPosts = async () => {
+    console.log('getPosts!')
+  
+    const q = lastPostSnapshot.current ? query(postsRef, orderBy('createdAt', 'desc'), startAfter(lastPostSnapshot.current), limit(10)) : query(postsRef, orderBy('createdAt', 'desc'), limit(10));
+    const snapshot = await getDocs(q);
+    const posts: Post[] = snapshot.docs.map(doc => doc.data());
+    setPosts(originPosts => [...originPosts, ...posts]);
+    setIsLast(snapshot.docs.length === 0);
+    
+    lastPostSnapshot.current = snapshot.docs[snapshot.docs.length - 1];
+  }
+
+  const handleBottom = () => {
+    getPosts();
   };
 
   return (
     <div>
-      <PostCardList posts={posts as Array<PostCardProps>} />
+      <button onClick={signout}>로그아웃</button>
+      <LoadingBox className="mb-16" onBottom={handleBottom}>
+        <PostCardList posts={posts} />
+      </LoadingBox>
       <PostInput onSubmit={handleSubmit} />
     </div>
   );
 }
 
-MainPage.propTypes = {
-};
-MainPage.defaultProps = {
-};
 
 export default MainPage;
