5724b96848db10b90916decab28702d11019500b
minung.han <hmu332233@gmail.com>
2022-01-23 17:43:34 +0900

    feat: 로그인 진행 중 멘트 변경

--- src/pages/SignInRedirect/index.tsx
+++ src/pages/SignInRedirect/index.tsx
@@ -1,9 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { auth } from '../../utils/firebase/firebase';
 import { isSignInWithEmailLink, signInWithEmailLink } from 'firebase/auth';
 
+const STEP_TEXT = {
+  PENDING: '로그인 중..',
+  COMPLETE: '로그인이 완료되었습니다. 이 페이지는 닫아주세요.'
+} as const;
+
+type Step = keyof typeof STEP_TEXT;
+
 function SignInRedirect() {
+  const [step, setStep] = useState<Step>('PENDING');
   useEffect(() => {
     // 이메일 링크를 사용한 로그인 링크인지 확인
     if (isSignInWithEmailLink(auth, window.location.href)) {
@@ -19,7 +27,7 @@ function SignInRedirect() {
         email = window.prompt('인증을 위해 이메일을 한번 더 입력해주세요!');
       }
       // The client SDK will parse the code from the link for you.
-      signInWithEmailLink(auth, email, window.location.href)
+      signInWithEmailLink(auth, email as string, window.location.href)
         .then((result) => {
           // Clear email from storage.
           window.localStorage.removeItem('emailForSignIn');
@@ -28,9 +36,7 @@ function SignInRedirect() {
           // result.additionalUserInfo.profile == null
           // You can check if the user is new or existing:
           // result.additionalUserInfo.isNewUser
-
-          // TODO: 로그인이 완료되었다고. 본래 페이지로 돌아가라고 하기
-          alert('로그인이 완료되었습니다. 본래 페이지로 돌아가세요!');
+          setStep('COMPLETE');
         })
         .catch((error) => {
           // Some error occurred, you can inspect the code: error.code
@@ -40,7 +46,11 @@ function SignInRedirect() {
     }
   }, []);
 
-  return <div>Sign in...</div>;
+  return (
+    <div className="text-center">
+      {STEP_TEXT[step]}
+    </div>
+  );
 }
 
 export default SignInRedirect;
