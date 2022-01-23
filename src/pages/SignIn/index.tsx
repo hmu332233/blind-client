29ef32a918417d18745a17ab483531837159ef7f
minung.han <hmu332233@gmail.com>
2022-01-24 00:05:23 +0900

    feat: 이메일 input에 required 추가

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -17,7 +17,6 @@ function SignIn(props: Props) {
     const { email } = Object.fromEntries(formData);
     signin(email as string);
 
-    console.log(location)
     const from = (location.state as any)?.from;
     navigate('/signin/complete', { replace: true, state: { from: from || '/' } });
     // e.currentTarget.reset();
@@ -27,7 +26,7 @@ function SignIn(props: Props) {
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="email" className="block text-sm font-bold text-gray-700">Email</label>
-        <input type="email" id="email" name="email" className="mt-1 block w-full shadow-md border-gray-300 rounded-md leading-6 px-2 py-3" />
+        <input type="email" id="email" name="email" className="mt-1 block w-full shadow-md border-gray-300 rounded-md leading-6 px-2 py-3" required />
         <button type="submit" className="bg-gray-900 text-white px-4 py-2 w-full fixed bottom-0 left-0">Sign In</button>
       </form>
     </div>
