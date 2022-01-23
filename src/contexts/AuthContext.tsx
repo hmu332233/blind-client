cfe290daa511cc1d96452dbd74de7f9ac2e1a9d6
minung.han <hmu332233@gmail.com>
2022-01-23 21:01:19 +0900

    feat: 로그인을 위한 host를 env로 분리

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,7 +6,7 @@ import { getAuth, onAuthStateChanged, signOut, sendSignInLinkToEmail } from 'fir
 const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: 'http://localhost:3000/signin/redirect',
+  url: `${process.env.REACT_APP_HOST}/signin/redirect`,
   // This must be true.
   handleCodeInApp: true,
 };
